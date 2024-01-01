b40144dd7ea3dfca4deee74d93f92f58f1b07078
timDeHof <this.is.tim.dehof@gmail.com>
2024-01-01 10:31:08 -0500

    initit commit

---
+++ src/slices/ImageBlock/index.tsx
@@ -0,0 +1,22 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `ImageBlock`.
+ */
+export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
+
+/**
+ * Component for "ImageBlock" Slices.
+ */
+const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
+  return (
+    <PrismicNextImage
+      field={slice.primary.image}
+      imgixParams={{ w: 600, q: 90 }}
+    />
+  );
+};
+
+export default ImageBlock;
